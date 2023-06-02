f6adc3210512b134618c235349758a17cbebc3bf
_dhruv <109019623+b-0-t@users.noreply.github.com>
2023-06-02 16:35:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import requests
 import json
 import os
 
+import ipywidgets as widgets
+from IPython.display import display
+from queue import Queue
+from threading import Thread
+
+
 # title and favicon ------------------------------------------------------------
 
 st.set_page_config(
@@ -84,6 +90,7 @@ def main():
         page = st.radio("Select your mode", tuple(pages.keys()))
 
     pages[page]()
+	
 
 
 # endregion main
@@ -166,9 +173,54 @@ def demo():
     else:
         path_in = None
         st.stop()
+	
+	
 
 
 # Custom API key mode -------------------------------------------------
+
+messages = Queue()
+recordings = Queue()
+
+record_button = widgets.Button(
+    description='Record',
+    disabled=False,
+    button_style='success',
+    tooltip='Record',
+    icon='microphone'
+)
+
+stop_button = widgets.Button(
+    description='Stop',
+    disabled=False,
+    button_style='warning',
+    tooltip='Stop',
+    icon='stop'
+)
+
+output = widgets.Output()
+
+def start_recording(data):
+    messages.put(True)
+    
+    with output:
+        display("Starting...")
+        record = Thread(target=record_microphone)
+        record.start()
+        transcribe = Thread(target=speech_recognition, args=(output,))
+        transcribe.start()
+
+def stop_recording(data):
+    with output:
+        messages.get()
+        display("Stopped.")
+    
+record_button.on_click(start_recording)
+stop_button.on_click(stop_recording)
+
+display(record_button, stop_button, output)
+
+
 
 
 def API_key():
